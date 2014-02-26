2e52128fa85335bde0a03e06289c36aac93c6c1d
leoneljunior <junior.hpj@hotmail.com>
2014-02-26 16:44:39 -0300

    Projeto com a funcionalidades relacionados ao Owner do petClinic

--- src/main/java/br/ufc/quixada/npi/ui/Main.java
+++ src/main/java/br/ufc/quixada/npi/ui/Main.java
@@ -3,26 +3,38 @@ package br.ufc.quixada.npi.ui;
 import java.util.List;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import br.ufc.quixada.npi.service.ClinicService;
 
 import br.ufc.quixada.npi.model.Contato;
+import br.ufc.quixada.npi.model.Owner;
 import br.ufc.quixada.npi.service.ContatoService;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-		ContatoService cs = (ContatoService)ctx.getBean("contatoServiceImpl");
-		try {
-			cs.insere();
-		} catch (Exception e) {
-			System.out.println("Não foi possível realizar a inserção");
-		}
+		ClinicService cs = (ClinicService)ctx.getBean("clinicServiceImpl");
 		
-		List<Contato> l = cs.findAll();
+		Owner owner = new Owner();
+		
+		owner.setFirstName("Raimundo");
+		owner.setAddress("Rua José Josué da Costa");
+		owner.setCity("Dep. Irapuan Pinheiro");
+		owner.setLastName("Leonel");
+		owner.setTelephone("88548663");
+		//owner.setId(null);
+				
+		
+		cs.saveOwner(owner);
+		
+		
+		/*
+		List<Owner> l = (List<Owner>) cs.findOwnerByLastName("Leonel");
 
-	    for (Contato c : l) {
+	    for (Owner c : l) {
 	    	System.out.println(c);
 	    }
+	    */
 		ctx.close();
 	}
 	
